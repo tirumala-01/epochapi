d2d2d69b7161e0e8f270cec7870a1f34501f5a20
Tirumala <ks.tirumala@gmail.com>
2025-02-18 02:39:21 -0500

    redis cluster update

--- app/commons/redis_cache.py
+++ app/commons/redis_cache.py
@@ -3,13 +3,10 @@ from ..config import settings
 
 REDIS_HOST = settings.REDIS_HOST
 
+
 class RedisCache:
     async def connect(self):
-        pool = redis.ConnectionPool(
-            host=REDIS_HOST,
-            decode_responses=True
-        )
-        self.client = await redis.Redis(connection_pool=pool)
+        self.client = await redis.RedisCluster(host=REDIS_HOST, decode_responses=True, db=0)
 
     async def disconnect(self):
         await self.client.aclose()
