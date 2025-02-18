1be91a36ccb4832abe078d00f5f621498c7f36ec
Tirumala <ks.tirumala@gmail.com>
2025-02-18 01:08:34 -0500

    redis conn changes

--- app/commons/redis_cache.py
+++ app/commons/redis_cache.py
@@ -2,30 +2,17 @@ import redis.asyncio as redis
 from ..config import settings
 
 REDIS_HOST = settings.REDIS_HOST
-REDIS_PORT = settings.REDIS_PORT
-REDIS_USERNAME = settings.REDIS_USERNAME
-REDIS_PASSWORD = settings.REDIS_PASSWORD
-
 
 class RedisCache:
-    def __init__(self, host, port, username, password):
+    def __init__(self, host):
         self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
         self.decode_responses = True
 
     async def connect(self):
-        self.client = await redis.Redis(
-            host=self.host,
-            port=self.port,
-            username=self.username,
-            password=self.password,
-            decode_responses=self.decode_responses,
-        )
+        self.client = await redis.Redis(host=self.host,decode_responses=self.decode_responses)
 
     async def disconnect(self):
         await self.client.aclose()
 
 
-cache = RedisCache(REDIS_HOST, REDIS_PORT, REDIS_USERNAME, REDIS_PASSWORD)
+cache = RedisCache(REDIS_HOST)
