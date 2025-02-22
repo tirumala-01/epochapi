6f1924006d28eb4fb69903f09a53e11a3acba8ac
Tirumala <ks.tirumala@gmail.com>
2025-02-21 19:58:12 -0500

    added local redis connection

--- app/commons/redis_cache.py
+++ app/commons/redis_cache.py
@@ -2,11 +2,20 @@ import redis.asyncio as redis
 from ..config import settings
 
 REDIS_HOST = settings.REDIS_HOST
-
+ENVIRONMENT = settings.ENVIRONMENT
 
 class RedisCache:
     async def connect(self):
-        self.client = await redis.RedisCluster(host=REDIS_HOST, decode_responses=True, db=0)
+        if ENVIRONMENT == "local":
+            self.client = await redis.Redis(
+                host='localhost',
+                decode_responses=True,
+                db=0,
+            )
+        else:
+            self.client = await redis.RedisCluster(
+                host=REDIS_HOST, decode_responses=True, db=0
+            )
 
     async def disconnect(self):
         await self.client.aclose()
--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     REDIS_HOST: str
     DATABASE_URL: str
-
+    ENVIRONMENT: str = "production"
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
